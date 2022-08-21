949fc6df341fde982247e09bb7128f368306d9d8
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2022-08-21 14:07:16 +0530

    Create Array_Cyclically rotate an array by one.java

---
+++ Array_Cyclically rotate an array by one.java
@@ -0,0 +1,11 @@
+class Compute {
+    
+    public void rotate(int arr[], int n)
+    {
+        for(int i=1; i<n ; i++){
+            int temp = arr[n-i];
+            arr[n-i] =arr[n-i-1];
+            arr[n-i-1] = temp;
+        }
+    } 
+}
