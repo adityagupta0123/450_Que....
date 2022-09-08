9f029798b4c8d36efef1798c800e27bdf8ce51b4
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2022-09-08 18:34:35 +0530

    Create _Array_dp_jump.java

---
+++ _Array_dp_jump.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        // if( nums.length == 1)
+        //     return true;
+        // int max =0;
+        // for(int i =0 ; i< nums.length - 1 && max >= i; i++ ){
+        //     if( max < i + nums[i])
+        //         max= i + nums[i];
+        //     if(max >= nums.length - 1)
+        //         return true;
+        // }
+        // return false;
+        
+        int goal = nums.length -1;
+        for(int i =nums.length -1; i >= 0; i-- ){
+            if( i + nums[i] >= goal)
+                goal = i;
+        }
+        return goal == 0 ? true : false; 
+      
+    }
+}
+// jump mean is index + value 
