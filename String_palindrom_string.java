230fa47ae330c0ab443c71b9fda3b85ce8f474de
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2022-12-07 09:44:45 +0530

    Create String_palindrom_string.java

---
+++ String_palindrom_string.java
@@ -0,0 +1,14 @@
+class Solution {
+    int isPalindrome(String S) {
+        int n = S.length() - 1 ;
+	    String  ans = "";
+	    for(int i = n ; i >= 0; i--)
+	    {
+	        ans += S.charAt(i);
+	    }
+	    if(ans.equals(S))
+	        return 1;
+	    else
+	        return 0;
+    }
+};
